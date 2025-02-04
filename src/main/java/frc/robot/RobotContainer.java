b1321108265aa92eab597fb69d5e31eeee401a19
TrentChang <chienchuanchang@gmail.com>
2025-02-04 12:17:32 +0800

    adding position estimate subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,12 +18,14 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Positionestimate;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
     
     private final Limelight mlimelight = new Limelight();
+    private final Positionestimate mpositionestimate = new Positionestimate();
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
---
+++ src/main/java/frc/robot/subsystems/Positionestimate.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Positionestimate extends SubsystemBase {
+    private NetworkTable table;
+    private NetworkTableEntry tx;
+    private NetworkTableEntry ty;
+    private NetworkTableEntry ta;
+
+    private final double limelightMountAngleDegrees = 25.0;
+    private final double limelightLensHeightInches = 20.0;
+    private final double goalHeightInches = 60.0;
+
+    public Positionestimate() {
+        // Get the NetworkTable for Limelight
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+
+        // Get the tx, ty, and ta values from the Limelight
+        tx = table.getEntry("tx");
+        ty = table.getEntry("ty");
+        ta = table.getEntry("ta");
+    }
+
+    public double getTx() {
+        return tx.getDouble(0.0);
+    }
+
+    public double getTy() {
+        return ty.getDouble(0.0);
+    }
+
+    public double getTa() {
+        return ta.getDouble(0.0);
+    }
+
+    public double calculateDistanceToGoal() {
+        // Calculate the angle to the goal in degrees
+        double angleToGoalDegrees = limelightMountAngleDegrees + getTy();
+
+        // Convert angle to radians
+        double angleToGoalRadians = angleToGoalDegrees * (Math.PI / 180.0);
+
+        // Calculate the distance from Limelight to the goal in inches
+        return (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
+    }
+
+    @Override
+    public void periodic() {
+        // Display the distance and tx, ty, ta values on SmartDashboard
+        SmartDashboard.putNumber("Distance to Goal (inches)", calculateDistanceToGoal());
+        SmartDashboard.putNumber("LimelightX", getTx());
+        SmartDashboard.putNumber("LimelightY", getTy());
+        SmartDashboard.putNumber("LimelightArea", getTa());
+    }
+}
